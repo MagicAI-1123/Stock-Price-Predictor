f47b4ace6df5c90937f0d31d7e6fc5f18f273969
goldvlady <vladyslavabramov@outlook.com>
2023-12-10 01:59:01 +0000

    fix little

--- app/Models/News.py
+++ app/Models/News.py
@@ -4,11 +4,8 @@ from typing import List
 # Define a Pydantic model for the items in the newsData array
 class NewsItem(BaseModel):
     # Replace these fields with the actual structure of your news item
-    date: str
     stockName: str
-    stockDetail: str
     headlineInfo: str
-    url: str
 
 # Define a Pydantic model for the request body that includes the newsData array
 class NewsDataArray(BaseModel):
